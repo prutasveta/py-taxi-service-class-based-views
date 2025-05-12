b6f1f224a738767ab284d864c706bb366be5523b
prutasveta <svetapruta@gmail.com>
2025-05-12 13:43:23 +0200

    Solution2

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8ovil3xu6=eaoqd#-#&ricv159poh5_lgm*)-dfcjqe=yc"
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
